0ff949673cbc0814b13a52b36dc4536d9812d3d7
Paul Chambers <P.R.Chambers@soton.ac.uk>
2015-12-16 18:35:07 +0000

    Conda build wasnt including pkg data: changed include_package_data to true

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ setup(
     version="0.21",
     description='Scripted Aircraft Geometry Module based on Python-OCC',
     packages=find_packages(),
-#    include_package_data=True,
-    package_data={'coord_seligFmt': ['*.dat']},
+    include_package_data=True,
+    # package_data={'': ['*.dat']},
     author='Andras Sobester, Paul Chambers',
     author_email='A.Sobester@soton.ac.uk, P.R.Chambers@soton.ac.uk'
 )
