Made progress in engine. Fixed chord scaling problem in airfoil primitives

airconics/AirCONICStools.py
@@ -25,7 +25,7 @@ from OCC.BRepFeat import BRepFeat_SplitShape
 from OCC.TopoDS import TopoDS_Shape
 from OCC.gp import gp_Trsf, gp_Ax2, gp_Pnt, gp_Dir, gp_Vec, gp_Pln, gp_Ax3#, gp_Cone
 #from OCC.Precision import precision_Angular, precision_Confusion
-from OCC.GeomAbs import GeomAbs_G2
+from OCC.GeomAbs import GeomAbs_G2, GeomAbs_C3
 
 # FileIO libraries:
 from OCC.STEPControl import STEPControl_Writer, STEPControl_AsIs, \
@@ -300,7 +300,6 @@ def transform_nonuniformal(brep, factors, vec=[0,0,0], copy=False):
         ("factors should have [Fx, Fy, Fz] scaling factors: Found length ",
          len(factors))
     from OCC.gp import gp_GTrsf, gp_Mat, gp_XYZ
-
     M = np.diag(factors).flatten()
     trns_M = gp_Mat(*M)
 
@@ -316,7 +315,7 @@ def transform_nonuniformal(brep, factors, vec=[0,0,0], copy=False):
     return brep_trns.Shape()
 
 
-def coslin(TransitionPoint, NCosPoints=8, NLinPoints=8):
+def coslin(TransitionPoint, NCosPoints=24, NLinPoints=24):
     """Creates a series of abscissas with cosine spacing from 0 to a
     TransitionPoint and a linear spacing thereafter, up to 1. The
     TransitionPoint corresponds to pi. Distribution suitable for airfoils
@@ -402,9 +401,9 @@ def AddSurfaceLoft(objs, continuity=GeomAbs_G2, check_compatibility=True,
 
         if close_sections:
             crv = obj.GetObject()
-            if crv.IsClosed():
+            if crv.IsClosed() is False:
                 # Add Finite TE edge
-                TE = act.make_edge(crv.EndPoint(), crv.StartPoint())
+                TE = make_edge(crv.EndPoint(), crv.StartPoint())
                 edges.append(TE)
             
         generator.AddWire(BRepBuilderAPI_MakeWire(*edges).Wire())
@@ -462,6 +461,17 @@ def translate_topods_from_vector(brep_or_iterable, vec, copy=False):
     else:
         return [translate_topods_from_vector(brep_or_iterable, vec, copy) for i in brep_or_iterable]
 
+
+def Uniform_Points_on_Curve(curve, NPoints):
+    """Returns a list of uniformly spaced points on a curve"""
+    from OCC.GCPnts import GCPnts_UniformAbscissa
+    from OCC.GeomAdaptor import GeomAdaptor_Curve
+    try:
+        adapt = GeomAdaptor_Curve(curve)
+    except:
+        adapt = GeomAdaptor_Curve(curve.GetHandle())
+    absc = GCPnts_UniformAbscissa(adapt, NPoints)
+    return [adapt.Value(absc.Parameter(i)) for i in xrange(1, NPoints+1)]
 
 def rotate(brep, axe, degree, copy=False):
     '''
@@ -599,7 +609,7 @@ def make_vertex(*args):
 def make_ellipsoid(centre_pt, dx, dy, dz):
     """Creates an ellipsoid from non-uniformly scaled unit sphere"""
     from OCC.BRepPrimAPI import BRepPrimAPI_MakeSphere
-    sphere = BRepPrimAPI_MakeSphere(gp_Pnt(0,0,0), 1)
+    sphere = BRepPrimAPI_MakeSphere(gp_Pnt(0,0,0), 0.5)
     ellipsoid = transform_nonuniformal(sphere.Shape(), [dx, dy, dz], vec=centre_pt)
     return ellipsoid
     
@@ -800,7 +810,7 @@ def CutSect(Shape, SpanStation):
     return Section, Chord   
 
 
-def AddCone(BasePoint, height, Radius, direction=gp_Dir(1, 0, 0)):
+def AddCone(BasePoint, Radius, height, direction=gp_Dir(1, 0, 0)):
     """Generates a cone shape originating at BasePoint with base radius Radius,
     and defined by its Apex - points in the direction of 'direc'
     Paramters
@@ -816,24 +826,25 @@ def AddCone(BasePoint, height, Radius, direction=gp_Dir(1, 0, 0)):
         BasePoint = gp_Pnt(*BasePoint)
     except:
         pass
-    ax2 = gp_Ax2(BasePoint, gp_Dir(1, 0, 0))
+    ax2 = gp_Ax2(BasePoint, direction)
     cone = BRepPrimAPI_MakeCone(ax2, Radius, 0, height)
     return cone.Shape()
 
 
-def TrimShapebyPlane(Shape, Plane, vec=gp_Vec(0, -10, 0)):
-    """Trims an OCC shape by plane. Default trims the 'left' of the plane
+def TrimShapebyPlane(Shape, Plane, pnt=gp_Pnt(0, -10, 0)):
+    """Trims an OCC shape by plane. Default trims the negative y side of the
+    plane
     Parameters
     ----------
     Shape - TopoDS_Shape
     Plane - expect TopoDS_Face
-    pnts - point defining which side of the halfspace contains its mass
+    pnt - point defining which side of the halfspace contains its mass
     """
-    from OCC.BRepPrimAPI import BRepPrimAPI_MakePrism    
-    tool = BRepPrimAPI_MakePrism(Plane, vec).Shape()
+    from OCC.BRepPrimAPI import BRepPrimAPI_MakeHalfSpace   
+    tool = BRepPrimAPI_MakeHalfSpace(Plane, pnt).Solid()
     trimmed_shape = boolean_cut(Shape, tool)
     
-    return trimmed_shape, tool
+    return trimmed_shape
         
 
 def boolean_cut(shapeToCutFrom, cuttingShape):

airconics/engine.py
@@ -11,7 +11,7 @@ from airconics import primitives, AirCONICStools as act
 import numpy as np
 from airconics.examples.wing_example_transonic_airliner import *
 from OCC.GC import GC_MakeCircle
-from OCC.gp import gp_Pnt, gp_Vec
+from OCC.gp import gp_Pnt, gp_Vec, gp_OY
 from OCC.Geom import Handle_Geom_Circle
 
 
@@ -46,12 +46,12 @@ class Engine:
         SectionNo = 100
         
         # Draw the nacelle with the centre of the intake highlight circle in 0,0,0
-        Highlight = GC_MakeCircle(gp_Pnt(0,0,HighlightRadius),
-                                  gp_Pnt(0,-HighlightRadius,0),
-                                  gp_Pnt(0,0,-HighlightRadius)).Value().GetObject()
-        HighlightCutterCircle = GC_MakeCircle(gp_Pnt(0,0,HighlightRadius*1.5),
-                                              gp_Pnt(0,-HighlightRadius*1.5,0),
-                                              gp_Pnt(0,0,-HighlightRadius*1.5)).Value().GetObject()
+        Highlight = act.make_circle3pt([0,0,HighlightRadius],
+                                  [0,-HighlightRadius,0],
+                                  [0,0,-HighlightRadius]).GetObject()
+        HighlightCutterCircle = act.make_circle3pt([0,0,HighlightRadius*1.5],
+                                              [0,-HighlightRadius*1.5,0],
+                                              [0,0,-HighlightRadius*1.5]).GetObject()
         # Fan disk for CFD boundary conditions
         FanCircle = Handle_Geom_Circle.DownCast(
             Highlight.Translated(gp_Vec(MeanNacelleLength*0.25, 0, 0)))
@@ -73,47 +73,50 @@ class Engine:
         TailCone = act.AddCone(TailConeBasePoint, TailConeRadius, TailConeHeight)
         self._TailCone = TailCone
 #        # Spinner cone
-#        SpinnerConeBasePoint = [MeanNacelleLength*0.26, 0,0]
-#        SpinnerConeApex    = [MeanNacelleLength*0.08, 0, 0]
-#        SpinnerConeRadius    =  MeanNacelleLength*0.09
-#        Spinner = rs.AddCone(SpinnerConeBasePoint, SpinnerConeApex, SpinnerConeRadius)
-#    
-#        
+        SpinnerConeBasePoint = np.array([MeanNacelleLength*0.26, 0,0])
+        SpinnerConeHeight    = MeanNacelleLength*(0.26-0.08)
+        SpinnerConeRadius    = MeanNacelleLength*0.09
+        Spinner = act.AddCone(SpinnerConeBasePoint,  SpinnerConeRadius, 
+                              SpinnerConeHeight, direction=gp_Dir(-1, 0, 0))
+        self._Spinner = Spinner
+
 #        # Tilt the intake
-#        RotVec = rs.VectorCreate((0,0,0),(0,1,0))
-#        Highlight = rs.RotateObject(Highlight, (0,0,0), ScarfAngle, axis = RotVec)
-#        
+        RotAx = gp_OY()
+        Highlight.Rotate(RotAx, np.radians(self.ScarfAngle))
 #        # Set up the disk for separating the intake lip later
-#        HighlightCutterCircle = rs.RotateObject(HighlightCutterCircle, (0,0,0), ScarfAngle, axis = RotVec)
-#        HighlightCutterDisk = rs.AddPlanarSrf(HighlightCutterCircle)
-#        rs.DeleteObject(HighlightCutterCircle)
-#        rs.MoveObject(HighlightCutterDisk, (HighlightDepth, 0,0))
+        HighlightCutterCircle.Rotate(RotAx, np.radians(self.ScarfAngle))
+        HighlightCutterDisk = act.PlanarSurf(HighlightCutterCircle)
+
+
+        HighlightCutterDisk = translate_topods_from_vector(HighlightCutterDisk,
+                                                           gp_Vec(HighlightDepth, 0,0))
 #        
 #        # Build the actual airfoil sections to define the nacelle
-#        HighlightPointVector = rs.DivideCurve(Highlight, SectionNo)
-#        
-#        Sections = []
-#        TailPoints = []
-#        Rotation = 0
-#        Twist = 0
-#        AirfoilSeligName = 'goe613'
-#        SmoothingPasses = 1
-#    
-#        for HighlightPoint in HighlightPointVector:
-#            ChordLength = MeanNacelleLength - HighlightPoint.X
-#            Af = primitives.Airfoil(HighlightPoint,ChordLength, Rotation, Twist, SeligPath=airconics_setup.SeligPath)
-#            AfCurve,Chrd = primitives.Airfoil.AddAirfoilFromSeligFile(Af, AirfoilSeligName, SmoothingPasses)
-#            rs.DeleteObject(Chrd)
-#            P = rs.CurveEndPoint(AfCurve)
-#            list.append(TailPoints, P)
-#            AfCurve = act.AddTEtoOpenAirfoil(AfCurve)
-#            list.append(Sections, AfCurve)
-#            Rotation = Rotation + 360.0/SectionNo
-#        
-#        list.append(TailPoints, TailPoints[0])
+        HighlightPointVector = act.Uniform_Points_on_Curve(Highlight, SectionNo)
+        
+        Sections = []
+        TailPoints = []
+        Rotation = 0
+        Twist = 0
+        AirfoilSeligName = 'goe613'
+        SmoothingPasses = 1
+        
+        Rotations = np.linspace(0, 360, SectionNo)
+        
+        for i, pt in enumerate(HighlightPointVector[0:2]):
+            Chord = MeanNacelleLength - pt.X()
+            print(pt.X(), pt.Y(), pt.Z(), Chord)
+            Af = primitives.Airfoil([pt.X(), pt.Y(), pt.Z()], Chord,
+                                     Rotations[i], Twist,
+                                     SeligProfile=AirfoilSeligName)
+            Sections.append(Af)
+            TailPoints.append(Af.Curve.GetObject().EndPoint())
+            
+        self._sections = Sections
 #        
 #        # Build the actual nacelle OML surface
-#        EndCircle = rs.AddInterpCurve(TailPoints)
+#        EndCircle = act.points_to_bspline(TailPoints)
+#        self._EndCircle = EndCircle
 #        Nacelle = rs.AddSweep2([Highlight, EndCircle], Sections, closed = True)
 #        # Separate the lip
 #        Cowling, HighlightSection = rs.SplitBrep(Nacelle, HighlightCutterDisk, True)
@@ -155,15 +158,7 @@ class Engine:
 #        act.AssignMaterial(PylonLeft,"White_composite_external")
 #        act.AssignMaterial(PylonRight,"White_composite_external")
 #    
-#        # Clean-up
-#        rs.DeleteObject(HighlightCutterDisk)
-#        rs.DeleteObjects(Sections)
-#        rs.DeleteObject(EndCircle)
-#        rs.DeleteObject(Highlight)
-#        rs.DeleteObjects([PylonTop, PylonAfCurve, PylonChord, PylonTE])
-#        
-#        
-#        rs.Redraw()
+#        # TODO: Clean-up
 #        
 #        TFEngine = [Cowling, HighlightSection, TailCone, FanDisk, Spinner, BypassDisk]
 #        TFPylon = [PylonLeft, PylonRight, PylonAfSrf]
@@ -217,4 +212,10 @@ if __name__ == "__main__":
     display.DisplayShape(EngineSection, update=True, color='Black')    
     display.DisplayShape(eng1._FDisk, update=True)
     display.DisplayShape(eng1._TailCone)
+    display.DisplayShape(eng1._Spinner)
+#    display.DisplayShape(eng1._EndCircle)
+    for section in eng1._sections:
+        display.DisplayShape(section.Curve, update=True, color='blue')
+    
+    
     start_display()

airconics/primitives.py
@@ -155,7 +155,7 @@ class Airfoil:
             vals = line.split()    # vals[0] = x coord, vals[1] = y coord
             x[i] = float(vals[0])
             z[i] = float(vals[1])
-        return x*self.ChordLength, z*self.ChordLength
+        return x, z
 
     def _NACA4cambercurve(self, MaxCamberLocTenthChord, MaxCamberPercChord):
         """ Generates the camber curve of a NACA 4-digit airfoil
@@ -272,11 +272,6 @@ class Airfoil:
 
         xu, zu, xl, zl, Theta = self._camberplusthickness(ChordCoord, zcam,
                                                           dzcamdx, t)
-#        Scale points:
-        xu *= self.ChordLength
-        zu *= self.ChordLength
-        xl *= self.ChordLength
-        zl *= self.ChordLength
         # Leading edge radius
         RLE = 1.1019*(MaxThicknessPercChord/100.0)**2.0
         
@@ -303,12 +298,14 @@ class Airfoil:
 #        Twist:
         if self.Twist:
             Curve.Rotate(gp_OY(), -np.radians(self.Twist))
+            
+#        Scaling:
+        Curve.Scale(gp_Pnt(0,0,0), self.ChordLength)
         
 #        Translation:
         self.Curve = Handle_Geom_BSplineCurve_DownCast(Curve.Translated(
                                                         gp_Vec(*self.LE))
                                                        )
-        
         return None
 
     def AddAirfoilFromSeligFile(self, SeligProfile, Smoothing=1):
@@ -352,7 +349,7 @@ class Airfoil:
         x, z, xu, zu, xl, zl, RLE = self._NACA4digitPnts(MaxCamberPercChord,
                                                          MaxCamberLocTenthChord,
                                                          MaxThicknessPercChord)
-                                                         
+        self._surface_pts = np.vstack([x, z]).T                                                 
         self.Curve = self._fitAirfoiltoPoints(x, z)
 #        if 'Smoothing' in locals():
 #            self.SmoothingIterations = Smoothing
@@ -368,8 +365,6 @@ class Airfoil:
         
         self.Profile = {'CRM_Epsilon': str(CRM_Epsilon)}
         x, z = CRMfoil.CRMlinear(CRM_Epsilon)
-        x *= self.ChordLength
-        z *= self.ChordLength
         self.Curve = self._fitAirfoiltoPoints(x, z)
         # TODO: Smoothing..
 #        if 'Smoothing' in locals():

tests/test_AirCONICStools.py
@@ -9,7 +9,7 @@ import numpy as np
 
 
 def test_coslin():
-    abscissa = coslin(0.5)[0]
+    abscissa = coslin(0.5, 8, 8)[0]
     ans = np.array([0.0,
                     0.01253604390908819,
                     0.04951556604879043,