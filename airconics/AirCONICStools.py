Engine working. Added base class AirconicsShape. Fixed Chord scaling, but wing surface needs work

airconics/AirCONICStools.py
@@ -13,27 +13,44 @@ from OCC.GeomAPI import GeomAPI_PointsToBSpline, GeomAPI_IntCS, GeomAPI_Interpol
 from OCC.BRepBndLib import brepbndlib_Add
 from OCC.TColgp import TColgp_Array1OfPnt, TColgp_HArray1OfPnt, TColgp_Array1OfVec
 from OCC.TColStd import TColStd_HArray1OfBoolean
-from OCC.BRepOffsetAPI import BRepOffsetAPI_ThruSections, BRepOffsetAPI_MakePipeShell
+from OCC.BRepOffsetAPI import  (BRepOffsetAPI_ThruSections,
+                                BRepOffsetAPI_MakePipeShell)
 from OCC.BRepBuilderAPI import (BRepBuilderAPI_MakeWire,
                                 BRepBuilderAPI_MakeEdge,
                                 BRepBuilderAPI_Transform,
-                                BRepBuilderAPI_MakeFace)
-from OCC.BRepPrimAPI import BRepPrimAPI_MakeBox, BRepPrimAPI_MakeCone
-from OCC.BRepAlgoAPI import BRepAlgoAPI_Section
-from OCC.BRepFeat import BRepFeat_SplitShape
+                                BRepBuilderAPI_MakeFace,
+                                BRepBuilderAPI_GTransform)
+from OCC.BRepPrimAPI import (BRepPrimAPI_MakeBox, BRepPrimAPI_MakeCone,
+                             BRepPrimAPI_MakeHalfSpace,
+                             BRepPrimAPI_MakeSphere)
+
+from OCC.BRepAlgoAPI import BRepAlgoAPI_Section, BRepAlgoAPI_Cut
+#from OCC.BRepFeat import BRepFeat_SplitShape
 #from OCC.BRep import BRep_Tool_Surface
-from OCC.TopoDS import TopoDS_Shape
-from OCC.gp import gp_Trsf, gp_Ax2, gp_Pnt, gp_Dir, gp_Vec, gp_Pln, gp_Ax3#, gp_Cone
+from OCC.gp import (gp_Trsf, gp_Ax2, gp_Pnt, gp_Dir, gp_Vec, gp_Pln,
+                    gp_GTrsf, gp_Mat, gp_XYZ)
 #from OCC.Precision import precision_Angular, precision_Confusion
-from OCC.GeomAbs import GeomAbs_G2, GeomAbs_C3
+from OCC.GeomAbs import GeomAbs_G2, GeomAbs_C2
+from OCC.TopoDS import *
+from OCC.TopAbs import *
+from OCC.TopExp import TopExp_Explorer
+from OCC.GC import GC_MakeCircle, GC_MakeSegment
+from OCC.Approx import Approx_ChordLength
+from OCC.GCPnts import GCPnts_UniformAbscissa
+from OCC.GeomAdaptor import GeomAdaptor_Curve, GeomAdaptor_HCurve
+from OCC.GeomPlate import (GeomPlate_CurveConstraint,
+                           GeomPlate_BuildPlateSurface,
+                           GeomPlate_MakeApprox)
+from OCC.BRepAdaptor import BRepAdaptor_Curve
+
 
 # FileIO libraries:
-from OCC.STEPControl import STEPControl_Writer, STEPControl_AsIs, \
-                             STEPControl_Reader
+from OCC.STEPControl import (STEPControl_Writer,
+                             STEPControl_AsIs,
+                             STEPControl_Reader)
 from OCC.Interface import Interface_Static_SetCVal
-from OCC.IFSelect import IFSelect_RetDone  #, IFSelect_ItemsByEntity
-from OCC.TopoDS import TopoDS_Shape
-from OCC.GC import GC_MakeCircle
+from OCC.IFSelect import IFSelect_RetDone
+
 
 # Standard Python libraries
 import numpy as np
@@ -179,8 +196,8 @@ def point_array_to_TColgp_PntArrayType(array, _type=TColgp_Array1OfPnt):
     return pt_arr
 
 
-def points_to_bspline(pnts, deg=3, periodic=False, tangents=None, tangent_method=0,
-                      scale=False):
+def points_to_bspline(pnts, deg=3, periodic=False, tangents=None,
+                      scale=False, continuity=GeomAbs_C2):
     """
     Points to bspline: originally from pythonocc-utils, changed to allow numpy
     arrays as input
@@ -211,7 +228,7 @@ def points_to_bspline(pnts, deg=3, periodic=False, tangents=None, tangent_method
         # Fit the curve to the point array
         deg_min = deg
         deg_max = deg
-        crv = GeomAPI_PointsToBSpline(pnts, deg_min, deg_max).Curve()
+        crv = GeomAPI_PointsToBSpline(pnts, deg_min, deg_max, continuity).Curve()
     else:
         _type = TColgp_HArray1OfPnt
         pnts = point_array_to_TColgp_PntArrayType(pnts, _type)
@@ -245,17 +262,7 @@ def points_to_BezierCurve(pnts):
         (with dimensions (Npoints x 3)), python list with elements [xi, yi, zi]
         or list of OCC.gp.gp_Pnt objects
     """
-    if type(pnts) is np.ndarray:
-        pnts = point_array_to_TColgp_PntArrayType(pnts)
-    elif type(pnts) is list:
-        if type(pnts[0]) is gp_Pnt:
-            pass
-        else:
-            assert(len(pnts[0]) == 3),  ("""Points should have length 3 (x, y, z).
-                  Found {}""".format(len(pnts[0])))
-            pnts = [gp_Pnt(pt) for pt in pnts]
-        pnts = point_list_to_TColgp_Array1OfPnt(pnts)
-
+    pnts = point_array_to_TColgp_PntArrayType(pnts, TColgp_Array1OfPnt)
     # Fit the curve to the point array
     crv = Geom_BezierCurve(pnts)
     return crv
@@ -299,7 +306,6 @@ def transform_nonuniformal(brep, factors, vec=[0,0,0], copy=False):
     assert(len(factors) == 3),\
         ("factors should have [Fx, Fy, Fz] scaling factors: Found length ",
          len(factors))
-    from OCC.gp import gp_GTrsf, gp_Mat, gp_XYZ
     M = np.diag(factors).flatten()
     trns_M = gp_Mat(*M)
 
@@ -309,7 +315,6 @@ def transform_nonuniformal(brep, factors, vec=[0,0,0], copy=False):
         V = gp_XYZ(vec.X(), vec.Y(), vec.Z())
     trns = gp_GTrsf(trns_M, V)
 
-    from OCC.BRepBuilderAPI import BRepBuilderAPI_GTransform
     brep_trns = BRepBuilderAPI_GTransform(brep, trns, copy)
     brep_trns.Build()
     return brep_trns.Shape()
@@ -360,7 +365,8 @@ def export_STEPFile(shapes, filename):
 
 
 def AddSurfaceLoft(objs, continuity=GeomAbs_G2, check_compatibility=True,
-                   solid=True, first_vertex=None, max_degree=8, close_sections=True):
+                   solid=True, first_vertex=None, last_vertex = None,
+                   max_degree=8, close_sections=True):
     """Create a lift surface through curve objects
     Parameters
     ----------
@@ -379,9 +385,7 @@ def AddSurfaceLoft(objs, continuity=GeomAbs_G2, check_compatibility=True,
     generator = BRepOffsetAPI_ThruSections(*args)
     generator.SetMaxDegree(max_degree)
 #    from OCC.GeomAbs import GeomAbs_G1
-    from OCC.Approx import Approx_ChordLength
     generator.SetParType(Approx_ChordLength)
-#    generator.SetContinuity(GeomAbs_G1)
     if first_vertex:
         generator.AddVertex(first_vertex)
     for obj in objs:
@@ -409,6 +413,8 @@ def AddSurfaceLoft(objs, continuity=GeomAbs_G2, check_compatibility=True,
         generator.AddWire(BRepBuilderAPI_MakeWire(*edges).Wire())
 #        else:
 #            generator
+    if last_vertex:
+        generator.AddVertex(last_vertex)
 
     generator.CheckCompatibility(check_compatibility)
     generator.SetContinuity(continuity)
@@ -464,12 +470,11 @@ def translate_topods_from_vector(brep_or_iterable, vec, copy=False):
 
 def Uniform_Points_on_Curve(curve, NPoints):
     """Returns a list of uniformly spaced points on a curve"""
-    from OCC.GCPnts import GCPnts_UniformAbscissa
-    from OCC.GeomAdaptor import GeomAdaptor_Curve
     try:
         adapt = GeomAdaptor_Curve(curve)
     except:
-        adapt = GeomAdaptor_Curve(curve.GetHandle())
+        # Allow the algorithm to deal with TopoDS_Edge and Wire shapes:
+        adapt = BRepAdaptor_Curve(curve)
     absc = GCPnts_UniformAbscissa(adapt, NPoints)
     return [adapt.Value(absc.Parameter(i)) for i in xrange(1, NPoints+1)]
 
@@ -588,13 +593,20 @@ def make_face(*args):
         return result
 
 
-def make_pipe_shell(spine, profiles):
+def make_pipe_shell(spine, profiles, support=None):
+    try:
+        spine = make_wire(make_edge(spine))
+    except:
+        pass
     pipe = BRepOffsetAPI_MakePipeShell(spine)
-    try: 
-        for profile in profiles:
+    for profile in profiles:
+        try:
             pipe.Add(profile)
-    except TypeError:
-        pipe.Add(profiles)
+        except:
+            wire = make_wire(make_edge(profile))
+            pipe.Add(wire)
+    if support:
+        pipe.SetMode(support)
     pipe.Build()
     with assert_isdone(pipe, 'failed building pipe'):
         return pipe.Shape()
@@ -608,7 +620,6 @@ def make_vertex(*args):
 
 def make_ellipsoid(centre_pt, dx, dy, dz):
     """Creates an ellipsoid from non-uniformly scaled unit sphere"""
-    from OCC.BRepPrimAPI import BRepPrimAPI_MakeSphere
     sphere = BRepPrimAPI_MakeSphere(gp_Pnt(0,0,0), 0.5)
     ellipsoid = transform_nonuniformal(sphere.Shape(), [dx, dy, dz], vec=centre_pt)
     return ellipsoid
@@ -700,16 +711,7 @@ def points_from_intersection(plane, curve):
         else:
             return None
 
-# This section is experimental
-#from OCC.BRepFill import BRepFill_Filling
-#from OCC.GeomAbs import GeomAbs_C0
-from OCC.GeomPlate import (GeomPlate_CurveConstraint, GeomPlate_BuildPlateSurface,
-                           GeomPlate_MakeApprox)
-from OCC.GeomAdaptor import GeomAdaptor_Curve, GeomAdaptor_HCurve
 
-from OCC.BRepAdaptor import BRepAdaptor_HCurve
-from OCC.BRepFill import BRepFill_CurveConstraint
-from OCC.Geom import Handle_Geom_Surface
 def Add_Network_Surface(curvenet, deg=3, initsurf=None):
     '''
     curvenet - list of Handle_GeomCurve
@@ -736,6 +738,7 @@ def Add_Network_Surface(curvenet, deg=3, initsurf=None):
 
     
     for curve in curvenet:
+        print(type(curve))
         adaptor = GeomAdaptor_Curve(curve)
         Hadapter = GeomAdaptor_HCurve(adaptor)
         constr = GeomPlate_CurveConstraint(Hadapter.GetHandle(), 0)
@@ -763,51 +766,48 @@ def Add_Network_Surface(curvenet, deg=3, initsurf=None):
         return face
     
 def CutSect(Shape, SpanStation):
-    # SpanStation is assumed to be along the y direction, in the range [0,1]
+    """ SpanStation is assumed to be along the y direction, in the range [0,1]
+    """
     
     (Xmin,Ymin,Zmin,Xmax,Ymax,Zmax) = ObjectsExtents([Shape])
     
     YStation = Ymin + (Ymax-Ymin)*SpanStation
     OriginX = Xmin -1
     OriginZ = Zmin -1
-    
-
+        
     P = gp_Pln(gp_Pnt(OriginX, YStation, OriginZ), gp_Dir(gp_Vec(0, 1, 0))) 
-    CutPlaneSrf = make_face(P, OriginZ, Zmax+1, OriginX, Xmax+1)
-
-    
-#    CutPlane = rs.PlaneFromPoints((OriginX, YStation, OriginZ), (Xmax+1, YStation, OriginZ), (OriginX, YStation, Zmax + 1))
-#    CutPlaneSrf = rs.AddPlaneSurface(CutPlane, max([(Xmax-Xmin),(Ymax-Ymin),(Zmax-Zmin)])+1, max([(Xmax-Xmin),(Ymax-Ymin),(Zmax-Zmin)])+1 )
-
+    # Note: using 2*extents here as previous +1 trimmed plane too short
+    CutPlaneSrf = make_face(P, 0, 2*Zmax, 0, 2*Xmax)
 
 
     I = BRepAlgoAPI_Section(Shape, CutPlaneSrf)
+    I.ComputePCurveOn1(True)
+    I.Approximation(True)
     I.Build()
     Section = I.Shape()
 
     (Xmin,Ymin,Zmin,Xmax,Ymax,Zmax) = ObjectsExtents([Section])
     
-#    
-#    # Find the apparent chord of the section (that is, the line connecting the fore
-#    # most and aftmost points on the curve
-#    DivPoints = rs.DivideCurve(Section, 200)
-#
-#    Xs = []
-#    Ys = []
-#    Zs = []
-#    for DP in DivPoints:
-#        list.append(Xs, DP[0])
-#        list.append(Ys, DP[1])
-#        list.append(Zs, DP[2])
-#        
-#    val, idx = min((val, idx) for (idx, val) in enumerate(Xs))
-#    LeadingPoint = [Xs[idx], Ys[idx], Zs[idx]]
-#    val, idx = max((val, idx) for (idx, val) in enumerate(Xs))
-#    TrailingPoint = [Xs[idx], Ys[idx], Zs[idx]]
-#    
-#    Chord = rs.AddLine(TrailingPoint, LeadingPoint)
-    Chord = None
-    return Section, Chord   
+#     Currently assume only one edge exists in the intersection:
+    exp = TopExp_Explorer(Section, TopAbs_EDGE)
+    edge = topods_Edge(exp.Current())    
+    
+#    Find the apparent chord of the section (that is, the line connecting the fore
+#    most and aftmost points on the curve
+    DivPoints = Uniform_Points_on_Curve(edge, 200)
+
+    Xs = [pt.X() for pt in DivPoints] 
+        
+    val, idx = min((val, idx) for (idx, val) in enumerate(Xs))
+    LeadingPoint = gp_Pnt(Xs[idx], DivPoints[idx].Y(),
+                          DivPoints[idx].Z())
+    
+    val, idx = max((val, idx) for (idx, val) in enumerate(Xs))
+    TrailingPoint = gp_Pnt(Xs[idx], DivPoints[idx].Y(),
+                           DivPoints[idx].Z())
+    
+    Chord = GC_MakeSegment(TrailingPoint, LeadingPoint).Value().GetObject()
+    return Section, Chord
 
 
 def AddCone(BasePoint, Radius, height, direction=gp_Dir(1, 0, 0)):
@@ -840,7 +840,6 @@ def TrimShapebyPlane(Shape, Plane, pnt=gp_Pnt(0, -10, 0)):
     Plane - expect TopoDS_Face
     pnt - point defining which side of the halfspace contains its mass
     """
-    from OCC.BRepPrimAPI import BRepPrimAPI_MakeHalfSpace   
     tool = BRepPrimAPI_MakeHalfSpace(Plane, pnt).Solid()
     trimmed_shape = boolean_cut(Shape, tool)
     
@@ -849,7 +848,6 @@ def TrimShapebyPlane(Shape, Plane, pnt=gp_Pnt(0, -10, 0)):
 
 def boolean_cut(shapeToCutFrom, cuttingShape):
     """Boolean cut tool from PythonOCC-Utils"""
-    from OCC.BRepAlgoAPI import BRepAlgoAPI_Cut
     try:
         cut = BRepAlgoAPI_Cut(shapeToCutFrom, cuttingShape)
         print 'can work?', cut.BuilderCanWork()

airconics/engine.py
@@ -11,12 +11,13 @@ from airconics import primitives, AirCONICStools as act
 import numpy as np
 from airconics.examples.wing_example_transonic_airliner import *
 from OCC.GC import GC_MakeCircle
-from OCC.gp import gp_Pnt, gp_Vec, gp_OY
+from OCC.gp import gp_Pnt, gp_Vec, gp_OY, gp_Dir, gp_Ax1
 from OCC.Geom import Handle_Geom_Circle
 
 
+
 # Will add a base class for rotating engine etc. later for inheritance
-class Engine:
+class Engine(primitives.AirconicsShape):
     def __init__(self,
                  EngineSection,
                  Chord, 
@@ -25,6 +26,10 @@ class Engine:
                  HighlightRadius=1.45,
                  MeanNacelleLength=5.67):
         """Constructor for the engine class"""
+        
+        #Inherit Base class variables
+        super(Engine, self).__init__()
+        
         self.EngineSection = EngineSection
         self.Chord = Chord
         self.CentreLocation = CentreLocation
@@ -34,53 +39,55 @@ class Engine:
         self.Build_Engine()
         
     def Build_Engine(self):
-        self.Engine, self.Pylon = self._TurbofanNacelle()
+        self.BuildTurbofanNacelle()
         return None
     
-    def _TurbofanNacelle(self):
+    def BuildTurbofanNacelle(self):
         """The defaults yield a nacelle similar to that of an RR Trent 1000 / GEnx
         """
+        CentreLocation = self.CentreLocation
+
         MeanNacelleLength = self.MeanNacelleLength
         HighlightRadius = self.HighlightRadius
         HighlightDepth = 0.12 * self.MeanNacelleLength
         SectionNo = 100
         
-        # Draw the nacelle with the centre of the intake highlight circle in 0,0,0
+#         Draw the nacelle with the centre of the intake highlight circle in 0,0,0
         Highlight = act.make_circle3pt([0,0,HighlightRadius],
                                   [0,-HighlightRadius,0],
                                   [0,0,-HighlightRadius]).GetObject()
         HighlightCutterCircle = act.make_circle3pt([0,0,HighlightRadius*1.5],
                                               [0,-HighlightRadius*1.5,0],
                                               [0,0,-HighlightRadius*1.5]).GetObject()
-        # Fan disk for CFD boundary conditions
+#         Fan disk for CFD boundary conditions
         FanCircle = Handle_Geom_Circle.DownCast(
             Highlight.Translated(gp_Vec(MeanNacelleLength*0.25, 0, 0)))
         wire = act.make_wire(act.make_edge(FanCircle))
         FanDisk = act.make_face(wire)
-        self._FC = FanCircle
-        self._FDisk = FanDisk
+        self.Components['FanDisk'] = FanDisk
         
-#        # Aft outflow for CFD boundary conditions
+#         Aft outflow for CFD boundary conditions
         BypassCircle = Handle_Geom_Circle.DownCast(
             Highlight.Translated(gp_Vec(MeanNacelleLength*0.85, 0, 0)))
         wire = act.make_wire(act.make_edge(BypassCircle))
         BypassDisk = act.make_face(wire)#        rs.DeleteObjects([FanCircle, BypassCircle])
+        self.Components['BypassDisk'] = BypassDisk
 #    
-#        # Outflow cone
+#         Outflow cone
         TailConeBasePoint = np.array([MeanNacelleLength*0.84, 0,0])
-        TailConeHeight    = MeanNacelleLength*1.35
+        TailConeHeight    = MeanNacelleLength*(1.35-0.84)
         TailConeRadius    =  HighlightRadius*0.782
         TailCone = act.AddCone(TailConeBasePoint, TailConeRadius, TailConeHeight)
-        self._TailCone = TailCone
-#        # Spinner cone
+        self.Components['TailCone'] = TailCone
+#         Spinner cone
         SpinnerConeBasePoint = np.array([MeanNacelleLength*0.26, 0,0])
         SpinnerConeHeight    = MeanNacelleLength*(0.26-0.08)
         SpinnerConeRadius    = MeanNacelleLength*0.09
         Spinner = act.AddCone(SpinnerConeBasePoint,  SpinnerConeRadius, 
                               SpinnerConeHeight, direction=gp_Dir(-1, 0, 0))
-        self._Spinner = Spinner
+        self.Components['Spinner'] = Spinner
 
-#        # Tilt the intake
+#         Tilt the intake
         RotAx = gp_OY()
         Highlight.Rotate(RotAx, np.radians(self.ScarfAngle))
 #        # Set up the disk for separating the intake lip later
@@ -88,83 +95,161 @@ class Engine:
         HighlightCutterDisk = act.PlanarSurf(HighlightCutterCircle)
 
 
-        HighlightCutterDisk = translate_topods_from_vector(HighlightCutterDisk,
+        HighlightCutterDisk = act.translate_topods_from_vector(HighlightCutterDisk,
                                                            gp_Vec(HighlightDepth, 0,0))
 #        
-#        # Build the actual airfoil sections to define the nacelle
-        HighlightPointVector = act.Uniform_Points_on_Curve(Highlight, SectionNo)
+#         Build the actual airfoil sections to define the nacelle
+        HighlightPointVector = act.Uniform_Points_on_Curve(Highlight.GetHandle(), SectionNo)
         
         Sections = []
         TailPoints = []
-        Rotation = 0
         Twist = 0
         AirfoilSeligName = 'goe613'
-        SmoothingPasses = 1
         
         Rotations = np.linspace(0, 360, SectionNo)
         
-        for i, pt in enumerate(HighlightPointVector[0:2]):
-            Chord = MeanNacelleLength - pt.X()
-            print(pt.X(), pt.Y(), pt.Z(), Chord)
-            Af = primitives.Airfoil([pt.X(), pt.Y(), pt.Z()], Chord,
+        for i, pt in enumerate(HighlightPointVector):
+            AfChord = MeanNacelleLength - pt.X()
+            Af = primitives.Airfoil([pt.X(), pt.Y(), pt.Z()], AfChord,
                                      Rotations[i], Twist,
-                                     SeligProfile=AirfoilSeligName)
+                                     SeligProfile=AirfoilSeligName).Curve
             Sections.append(Af)
-            TailPoints.append(Af.Curve.GetObject().EndPoint())
+            TailPoints.append(Af.GetObject().EndPoint())
             
         self._sections = Sections
 #        
 #        # Build the actual nacelle OML surface
-#        EndCircle = act.points_to_bspline(TailPoints)
+#        EndCircle = act.points_to_bspline(TailPoints)    #Dont need this?
 #        self._EndCircle = EndCircle
-#        Nacelle = rs.AddSweep2([Highlight, EndCircle], Sections, closed = True)
-#        # Separate the lip
-#        Cowling, HighlightSection = rs.SplitBrep(Nacelle, HighlightCutterDisk, True)
+        Nacelle =  act.AddSurfaceLoft(Sections)
+        self.Components['Nacelle'] = Nacelle
+#        TODO: Separate the lip
+#        Cowling, HighlightSection = act.TrimShapebyPlane(Nacelle, HighlightCutterDisk, True)
+
+#        Move the engine into its actual place on the wing (Fuse first?)
+        self.TranslateComponents(gp_Vec(*CentreLocation))         
 #        
-#        
-#        # Now build the pylon between the engine and the specified chord on the wing
-#        CP1 = [MeanNacelleLength*0.26+CentreLocation[0],CentreLocation[1],CentreLocation[2]+HighlightRadius*0.1]
-#        CP2 = [MeanNacelleLength*0.4+CentreLocation[0],CentreLocation[1],HighlightRadius*1.45+CentreLocation[2]]
-#        CP3 = rs.CurveEndPoint(Chord)
-#        rs.ReverseCurve(Chord)
-#        CP4 = rs.CurveEndPoint(Chord)
+#        Now build the pylon between the engine and the specified chord on the wing
+        CP1 = gp_Pnt(MeanNacelleLength*0.26+CentreLocation[0],
+                     CentreLocation[1], CentreLocation[2]+HighlightRadius*0.1)
+        CP2 = gp_Pnt(MeanNacelleLength*0.4+CentreLocation[0], CentreLocation[1],
+               HighlightRadius*1.45+CentreLocation[2])
+        CP3 = self.Chord.EndPoint()
+        CP4 = self.Chord.StartPoint()
+#        CP4 = gp_Pnt((CP3.X()+CP5.X())/2., 
+#                     (CP3.Y()+CP5.Y())/2.,
+#                     (CP3.Z()+CP5.Z())/2.)# Adding a mid point to make the curve behave better
+        self._pylonPts = [CP1, CP2, CP3, CP4]
 #    
-#        # Move the engine into its actual place on the wing
-#        rs.MoveObjects([HighlightSection, Cowling, FanDisk, BypassDisk, TailCone, Spinner], CentreLocation)
-#    
-#        # Pylon wireframe
-#        PylonTop = rs.AddInterpCurve([CP1, CP2, CP3, CP4])
-#        PylonAf = primitives.Airfoil(CP1,MeanNacelleLength*1.35, 90, 0, airconics_setup.SeligPath)
-#        PylonAfCurve,PylonChord = primitives.Airfoil.AddNACA4(PylonAf, 0, 0, 12, 3)
+
+        
+ 
+#        Pylon wireframe
+        tangents = np.array([[0,0,1],[1, 0, 0]])
+        PylonTop = act.points_to_bspline([CP1, CP2, CP3, CP4], tangents=tangents)
+        self._PylonTop = PylonTop
+        PylonBase_LE = [CP1.X(), CP1.Y(), CP1.Z()]
+        PylonAf = primitives.Airfoil(PylonBase_LE,MeanNacelleLength*1.35, 90,
+                                     0, Naca4Profile='0012', EnforceSharpTE=False)
+        self._PylonAf = PylonAf
+        LowerTE = PylonAf.ChordLine.GetObject().EndPoint()
 #        LowerTE = rs.CurveEndPoint(PylonChord)
-#        PylonTE = rs.AddLine(LowerTE, CP4) 
-#    
-#        # Create the actual pylon surface
-#        PylonLeft = rs.AddNetworkSrf([PylonTop, PylonAfCurve, PylonTE])
-#        rs.MoveObject(PylonLeft, (0,-CentreLocation[1],0))
-#        PylonRight = act.MirrorObjectXZ(PylonLeft)
-#        rs.MoveObject(PylonLeft,  (0,CentreLocation[1],0))
-#        rs.MoveObject(PylonRight, (0,CentreLocation[1],0))
+        from OCC.GC import GC_MakeSegment
+        PylonTE = GC_MakeSegment(LowerTE, CP4).Value()
+        self._PylonTE = PylonTE
+#        # Create the actual pylon surface       
+        # Method 1: Sweep - gives the wrong shape
+#        Pylon_tip = GC_MakeCircle(gp_Ax1(CP5, gp_Dir(0,1,0)), PylonAf.ChordLength*0.001).Value()
+
+        Pylon_curve = PylonAf.Curve.GetObject()
+        PylonAf_TE = act.make_edge(Pylon_curve.StartPoint(), Pylon_curve.EndPoint())
+#        PylonAf_Face = act.PlanarSurf(PylonAf.Curve)
+        
+        
+#        PylonAf_closedwire = act.make_wire(act.make_edge(PylonAf.Curve), PylonAf_TE)
+#         Add a centre plane and trim the curve
+        edges = [act.make_edge(PylonAf.ChordLine),
+                 act.make_edge(PylonTop),
+                 act.make_edge(PylonTE)]
+        trimplane = act.make_face(act.make_wire(*edges))
+#        wire = act.TrimShapebyPlane(PylonAf_closedwire,trimplane, pnt=gp_Pnt(0, -10, 0))
+        self.Components['Pylon_Flatplate'] = trimplane
+#        self._temp_halfsection = wire
+        
+#        sections = [PylonAf.Curve, PylonTE]
+#        spine = act.make_wire(act.make_edge(PylonTop))
+##        support = act.make_wire(act.make_edge(PylonTE))
+##        self.PylonLeft = act.make_pipe_shell(spine, sections)
+#        from OCC.BRepOffsetAPI import BRepOffsetAPI_MakePipeShell
+#     
+#        builder = BRepOffsetAPI_MakePipeShell(spine)
+#        builder.C
+#        builder.Add(PylonAf_closedwire)
+#  
+##        poly = BRepBuilderAPI_MakePolygon(p1, p2, p3).Wire()
+#        
+#        
+#        builder.Add(act.make_vertex(CP5))
+#        builder.Build()
+#        self.PylonLeft = builder.Shape()
+#        Tip circle:
+        
+#        self.PylonLeft = act.Add_Network_Surface([wire] + edges)
+#        sections = 
+#        spine = act.make_wire(act.make_edge(PylonTop))
+#        support = act.make_wire(act.make_edge(PylonTE))
+        
+#        Method 3: Evolved
+#        from OCC.BRepOffsetAPI import BRepOffsetAPI_MakeEvolved
+#        from OCC.GeomAbs import GeomAbs_C0, GeomAbs_G2
+#
+#        print(type(wire))
+#        continuity = GeomAbs_G2
+#        filler = BRepOffsetAPI_MakeEvolved(act.make_wire(act.make_edge(PylonTop)), sections)
+##        filler.Add(trimplane, GeomAbs_G2)
+#        
+##        filler.Add(e, continuity)
+##        for edge in edges:
+##            filler.Add(edge, continuity)
+#        filler.Build()
+#        face = filler.Shape()
+#        self.PylonLeft = face
+            
+
+        # Move into place under the wing
+#        self._Translate(gp_Vec(0,-CentreLocation[1],0))
+        
+        
+#        PylonRight = act.mirror(PylonLeft, plane='xz')
 #        PylonAfCurve = act.AddTEtoOpenAirfoil(PylonAfCurve)
 #        PylonAfSrf = rs.AddPlanarSrf(PylonAfCurve)
-#    
-#        # Assigning basic surface properties
-#        act.AssignMaterial(Cowling, "ShinyBABlueMetal")
-#        act.AssignMaterial(HighlightSection, "UnpaintedMetal")
-#        act.AssignMaterial(TailCone, "UnpaintedMetal")
-#        act.AssignMaterial(FanDisk, "FanDisk")
-#        act.AssignMaterial(Spinner, "ShinyBlack")
-#        act.AssignMaterial(BypassDisk, "FanDisk")
-#        act.AssignMaterial(PylonLeft,"White_composite_external")
-#        act.AssignMaterial(PylonRight,"White_composite_external")
-#    
+#
 #        # TODO: Clean-up
 #        
 #        TFEngine = [Cowling, HighlightSection, TailCone, FanDisk, Spinner, BypassDisk]
 #        TFPylon = [PylonLeft, PylonRight, PylonAfSrf]
+
+        
         TFEngine = None,
         TFPylon=None
         return TFEngine, TFPylon
+        
+
+    
+#    def Display(self, context, material=Graphic3d_NOM_ALUMINIUM):
+#        """Displays all components of the engine to input context
+#        Parameters
+#        ----------
+#        meterial - OCC.Graphic3d_NOM_* type
+#            The material for display: note some renderers do not allow this"""
+#        for component in self.Components:
+#            ais = AIS_Shape(self.Components[component])
+#            ais.SetMaterial(material)
+#            try:
+#                context.Context.Display(ais.GetHandle())
+#            except:
+#                context.DisplayShape(self.Components[component])
+#    
     
 if __name__ == "__main__":
     from OCC.Display.SimpleGui import init_display
@@ -172,9 +257,10 @@ if __name__ == "__main__":
 
 #    Generate a wing first to attach the engine to
     P = (0,0,0)
+
     SegmentNo = 10
     ChordFactor = 1
-    ScaleFactor = 50
+    ScaleFactor = 44.56
     Wing = liftingsurface.LiftingSurface(P, mySweepAngleFunctionAirliner, 
                                          myDihedralFunctionAirliner, 
                                          myTwistFunctionAirliner, 
@@ -189,10 +275,8 @@ if __name__ == "__main__":
     NacelleLength = 1.95*EngineDia
     
     EngineSection, Chord = act.CutSect(Wing.Shape, SpanStation)
-    print(type(EngineSection))
-#    CEP = rs.CurveEndPoint(Chord)
-    CEP = gp_Pnt(0, 0, 0)
-
+    CEP = Chord.EndPoint()
+    display.DisplayShape(Chord, update=True, color='black')
     # Variables controlling the position of the engine with respect to the wing
     EngineCtrFwdOfLE = 0.98  
     EngineCtrBelowLE = 0.35
@@ -208,14 +292,17 @@ if __name__ == "__main__":
                    HighlightRadius=EngineDia/2.0,
                    MeanNacelleLength = NacelleLength)
     
-    display.DisplayShape(act.make_edge(eng1._FC), update=True)
-    display.DisplayShape(EngineSection, update=True, color='Black')    
-    display.DisplayShape(eng1._FDisk, update=True)
-    display.DisplayShape(eng1._TailCone)
-    display.DisplayShape(eng1._Spinner)
-#    display.DisplayShape(eng1._EndCircle)
-    for section in eng1._sections:
-        display.DisplayShape(section.Curve, update=True, color='blue')
+#    Display Engine Components:
+    eng1.Display(display)
     
-    
+#    Display pylon components:
+    display.DisplayShape(eng1._PylonAf.Curve)
+    display.DisplayShape(eng1._PylonTop)
+    display.DisplayShape(eng1._PylonTE)  
+    display.DisplayShape(EngineSection, color='black')
+
+#    display.DisplayShape(eng1._Pylon_Flatplate)
+
+#    eng1.Display(display)
+
     start_display()

airconics/primitives.py
@@ -9,12 +9,54 @@ from pkg_resources import resource_string, resource_exists
 import numpy as np
 
 from OCC.Geom import Handle_Geom_BSplineCurve_DownCast
+from OCC.GC import GC_MakeSegment
 #from OCC.Geom2dAPI import Geom2dAPI_PointsToBSpline
 from OCC.gp import gp_Pnt, gp_Pnt2d, gp_Pln, gp_Dir, gp_Vec, gp_OX, gp_OY
 #from OCC.FairCurve import FairCurve_MinimalVariation
 
 import CRMfoil
 import airconics.AirCONICStools as act
+
+
+from OCC.Graphic3d import Graphic3d_NOM_ALUMINIUM
+from OCC.AIS import AIS_Shape
+import copy
+
+class AirconicsShape(object):
+    """Base class From which airconics assemblies and shapes will be made"""
+    def __init__(self):
+        """Defines an empty shape"""
+        self.Components = {}
+        
+    def TranslateComponents(self, vec):
+        """Just using this for now: will eventually fuse all shapes"""
+        for component in self.Components:
+            self.Components[component] = act.translate_topods_from_vector(self.Components[component], vec)
+           
+        
+    def Display(self, context, material=Graphic3d_NOM_ALUMINIUM):
+        """Displays all components of the engine to input context
+        Parameters
+        ----------
+        meterial - OCC.Graphic3d_NOM_* type
+            The material for display: note some renderers do not allow this"""
+        for component in self.Components:
+            ais = AIS_Shape(self.Components[component])
+            ais.SetMaterial(material)
+            try:
+                context.Context.Display(ais.GetHandle())
+            except:
+                context.DisplayShape(self.Components[component])
+    
+    def MirrorComponents(self, plane='xz'):
+        """Returns a mirrored version of this airconics shape"""
+        print("Note: MirrorComponents currently mirrors only the shape\n")
+        print("components, other attributes will not be mirrored\n")
+        mirrored = AirconicsShape()
+        for component in self.Components:
+            mirrored.Components[component] = act.mirror(
+                    self.Components[component], plane, copy=True)
+        return mirrored
 
 
 class Airfoil:
@@ -286,26 +328,37 @@ class Airfoil:
         # TODO: Smoothing
 #        for i in range(self.SmoothingIterations):
 #            rs.FairCurve(self.Curve)
+
+        # Can assume that the chord is from 0,0,0 to 1,0,0 before translation 
+        ChordLine = GC_MakeSegment(gp_Pnt(0,0,0),
+                                   gp_Pnt(1,0,0)).Value().GetObject()     
+        
         Curve = self.Curve.GetObject()
+
+#        Scaling:
+        Curve.Scale(gp_Pnt(0,0,0), self.ChordLength)
+        ChordLine.Scale(gp_Pnt(0,0,0), self.ChordLength)
 
 #        Rotations - Note that direction is opposite to Rhino
 #        Dihedral:
         if self.Rotation:
-            # self.Curve = Handle_Geom_BSplineCurve_DownCast(Curve.Rotate(A1))
             Curve.Rotate(gp_OX(), np.radians(self.Rotation))
-#            act.rotate(Curve, gp_OX(), -self.Rotation)
+            ChordLine.Rotate(gp_OX(), np.radians(self.Rotation))
 
 #        Twist:
         if self.Twist:
             Curve.Rotate(gp_OY(), -np.radians(self.Twist))
-            
-#        Scaling:
-        Curve.Scale(gp_Pnt(0,0,0), self.ChordLength)
-        
+            ChordLine.Rotate(gp_OY(), -np.radians(self.Twist))
+
 #        Translation:
-        self.Curve = Handle_Geom_BSplineCurve_DownCast(Curve.Translated(
-                                                        gp_Vec(*self.LE))
-                                                       )
+#        self.Curve = Handle_Geom_BSplineCurve_DownCast(Curve.Translated(
+#                                                        gp_Vec(*self.LE))
+#                                                       )
+        Curve.Translate(gp_Vec(*self.LE))
+        ChordLine.Translate(gp_Vec(*self.LE))
+        
+        self.ChordLine = ChordLine.GetHandle()
+
         return None
 
     def AddAirfoilFromSeligFile(self, SeligProfile, Smoothing=1):