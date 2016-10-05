025570a5b7d23ff8ba6800a33dc010164940b8ab
p-chambers <P.R.Chambers@soton.ac.uk>
2016-10-05 11:29:32 +0100

    Merging conda build information from other repository

---
+++ ci/conda/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Add DLR-SC channel if not present ?
+# conda config --add channels https://conda.anaconda.org/dlr-sc
+
+$PYTHON setup.py install
+
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
