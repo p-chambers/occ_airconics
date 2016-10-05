b976f7533e7c44f4d802cb3b4ca9019d51be5717
p-chambers <P.R.Chambers@soton.ac.uk>
2016-10-05 15:30:56 +0100

    minor changes to set_pkg_version: conda build not finding version

--- ci/conda/set_pkg_version.py
+++ ci/conda/set_pkg_version.py
@@ -2,12 +2,12 @@
 # @Author: p-chambers
 # @Date:   2016-10-05 14:35:02
 # @Last Modified by:   p-chambers
-# @Last Modified time: 2016-10-05 14:38:46
+# @Last Modified time: 2016-10-05 15:30:29
 
 import os
 from setuptools_scm import get_version
 
 
-version = get_version(root='..', relative_to=__file__)
+version = get_version(root=os.path.join('..', '..'), relative_to=__file__)
 
 os.environ['OCC_AIRCONICS_VERSION'] = version
