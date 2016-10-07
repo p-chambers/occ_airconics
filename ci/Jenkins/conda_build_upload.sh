b594a0e234cec45ae5433f98980e3af660cf34a4
p-chambers <P.R.Chambers@soton.ac.uk>
2016-10-07 16:18:27 +0100

    Added six package as install requirement. Adding test on python 3 to jenkins files

--- ci/Jenkins/conda_build_upload.sh
+++ ci/Jenkins/conda_build_upload.sh
@@ -7,7 +7,7 @@
 # a new tag is released in master. 
 #
 # @Last Modified by:   p-chambers
-# @Last Modified time: 2016-10-07 15:57:25
+# @Last Modified time: 2016-10-07 16:12:52
 
 ##############################################################################
 ## THIS PART IS THE SAME AS JENKINS_BUILD.SH, BUT IS INCLUDED HERE RATHER THAN
@@ -15,7 +15,7 @@
 ##############################################################################
 # Create the Conda test environment (note that pytest is required, otherwise
 # wrong python is used and the module is not found!)
-conda create --name occ_airconics_build python=2 pytest numpy
+conda create --name occ_airconics_build python=2 pytest
 source activate occ_airconics_build
 
 # Install the python-occ precompiled binary from DLR-SC with Conda
@@ -49,7 +49,7 @@ JEKINS_PLATFORM=linux-64
 CONDA_PYVERSION_FLAGS="--py 2.7 --py 3.5"
 
 # My jenkins is currently running on linux-64, so get the appropriate file:
-CONDA_PKG_NAME=$CONDA_BUILD_DIR/${JEKINS_PLATFORM}/occ_airconics-${GIT_DESCRIBE_TAG}*
+CONDA_PKG_NAME=$CONDA_BUILD_DIR/${JEKINS_PLATFORM}/occ_airconics-${GIT_DESCRIBE_TAG}-py27*
 
 echo "Making package ${CONDA_PKG_NAME_}"
 
@@ -60,9 +60,7 @@ echo 'Preparing to run tests'
 py.test -v --junitxml=pytest-report.xml
 
 # Convert to all platforms and output in the current folder
-
 conda convert --platform all $CONDA_PKG_NAME -o $PKG_OUTPUT_DIR
-
 
 # Clean the Conda test/build environment
 source deactivate
@@ -70,3 +68,27 @@ conda env remove -n occ_airconics_build
 
 # clean conda build directories
 conda build purge
+
+
+
+# Also test the package on python 3:
+conda create --name occ_airconics_py3build python=3 pytest
+source activate occ_airconics_py3build
+
+# Install the python-occ precompiled binary from DLR-SC with Conda
+conda install --name occ_airconics_py3build -c https://conda.anaconda.org/dlr-sc pythonocc-core
+
+CONDA_PKG_NAME=$CONDA_BUILD_DIR/${JEKINS_PLATFORM}/occ_airconics-${GIT_DESCRIBE_TAG}-py35*
+
+# Install and test
+conda install --use-local $CONDA_PKG_NAME
+
+echo 'Preparing to run tests'
+py.test -v --junitxml=pytest-report.xml
+
+# Convert to all platforms and output in the current folder
+conda convert --platform all $CONDA_PKG_NAME -o $PKG_OUTPUT_DIR
+
+# Clean the Conda test/build python 3 environment
+source deactivate
+conda env remove -n occ_airconics_py3build
