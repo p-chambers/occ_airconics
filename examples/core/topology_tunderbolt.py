0cd055813c3852334703b4eb0e23ab43df39d641
p-chambers <P.R.Chambers@soton.ac.uk>
2017-01-05 19:02:49 +0000

    Adding thunderbolt topology example (basic version).

---
+++ examples/core/topology_tunderbolt.py
@@ -0,0 +1,21 @@
+from airconics.topology import Topology_GPTools
+import os
+import airconics
+
+
+if __name__ == '__main__':
+    # Initialise the display
+    from OCC.Display.SimpleGui import init_display
+    display, start_display, add_menu, add_function_to_menu = init_display()
+
+    topo_tools = Topology_GPTools(MaxAttachments=4)
+
+    fname = os.path.join(os.path.dirname(airconics.__file__),
+                         'resources/configuration_app/presets/thunderbolt.json')
+
+    topo = topo_tools.from_JSONFile(fname)
+
+    topo.Display(display)
+
+    display.FitAll()
+    start_display()
